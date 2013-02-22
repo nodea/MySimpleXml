71442e9e7d41c090a9922ff2e0a158b7991b49c8
Raim FAHSSI <mfahssi@gmail.com>
2013-02-22 11:29:48 +0100

    add modification

--- dcll.truc/src/main/java/dcll/truc/mysimplexml/MyMojo.java
+++ dcll.truc/src/main/java/dcll/truc/mysimplexml/MyMojo.java
@@ -1,7 +1,24 @@
 package dcll.truc.mysimplexml;
 
+import java.io.IOException;
+
+import nu.xom.*;
+
+
 
 public class MyMojo
 {
-    
+	  public static void main(String[] args) throws ParsingException, IOException {
+	        Builder builder = new Builder();
+	        Document doc = builder.build("NezFile.xml");
+	        /*Element root = doc.getRootElement();
+	        Element utc = new Element("utc");
+	        Attribute att = new Attribute("offset", "-08:00");
+	        utc.addAttribute(att);
+	        root.insertChild(0, "\n ");
+	        root.insertChild(1, utc);
+	        root.removeChild(4);
+	        root.removeChild(4);*/
+	        System.out.println(doc.toXML());
+	  }
 }
